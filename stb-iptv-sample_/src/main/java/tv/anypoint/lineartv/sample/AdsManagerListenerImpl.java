f09ac651b59326228135722f9512fe79736651ef
Ryan <jh.bae@anypointmedia.com>
2024-11-15 14:39:01 +0900

    rename project name

---
+++ stb-iptv-sample_/src/main/java/tv/anypoint/lineartv/sample/AdsManagerListenerImpl.java
@@ -0,0 +1,43 @@
+package tv.anypoint.lineartv.sample;
+
+import android.app.Activity;
+import tv.anypoint.api.ads.AnypointAdsManager;
+
+class AdsManagerListenerImpl implements AnypointAdsManager.AdsManagerListener {
+    private final ChannelPlayer channelPlayer;
+
+    public AdsManagerListenerImpl(ChannelPlayer channelPlayer) {
+        this.channelPlayer = channelPlayer;
+    }
+
+    @Override
+    public boolean onPrepare(int i, boolean b) {
+        return false;
+    }
+
+    public void onPlay(boolean retainChannelStream) { channelPlayer.setVolume(0); }
+
+    @Override
+    public void onPause() {
+
+    }
+
+    @Override
+    public void onResume() {
+
+    }
+
+    @Override
+    public void prepareStop(boolean b) {
+
+    }
+
+    public void onStopped(boolean retainChannelStream) {
+        channelPlayer.setVolume(1);
+    }
+
+    @Override
+    public void onError(Throwable throwable, boolean b) {
+
+    }
+}
---
+++ stb-iptv-sample_/src/main/java/tv/anypoint/lineartv/sample/TVApplication.java
@@ -0,0 +1,38 @@
+package tv.anypoint.lineartv.sample;
+
+import android.app.Application;
+import android.util.Log;
+import tv.anypoint.api.AnypointSdk;
+import tv.anypoint.impl.common.SdkModuleLifecycleListener;
+
+public class TVApplication extends Application {
+    public static final String TAG = "ANYPOINT_SDK_SAMPLE";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        AnypointSdk.setDebugMode(true);
+        AnypointSdk.initialize(getApplicationContext());
+
+        boolean isInitialized = AnypointSdk.isInitialized();
+        if (!isInitialized) {
+        }
+
+        AnypointSdk.addModuleLifecycleListener(new SdkModuleLifecycleListener() {
+            @Override
+            public void initialized(String module) {
+                if (module.equals("multicast-exoplayer")) {
+                    Log.d(TAG, "initialized: ");
+                }
+            }
+        });
+    }
+
+    @Override
+    public void onTerminate() {
+        super.onTerminate();
+
+        AnypointSdk.destroy();
+    }
+}
